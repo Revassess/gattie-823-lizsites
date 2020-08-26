87d4d652327fd299fd29933d066151f6f1583afb
lizsites <69205363+lizsites@users.noreply.github.com>
2020-08-26 14:13:49 -0500

    should pass

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,22 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		String numString = Long.toString(num);
+		System.out.println("numString " +numString);
+		char[] numDigits = numString.toCharArray();
+		int[] intDigits  = new int[numDigits.length];
+		System.out.println("charrArray " + Character.toString(numDigits[numDigits.length -1]));
+		for (int i = 0; i < numDigits.length; i++) {
+			
+			intDigits[i] = Integer.parseInt(Character.toString(numDigits[i]));
+		}
+		int powers = 0;
+		int numSum = 0;
+		for (int i = 0; i < intDigits.length; i++) {
+			numSum += intDigits[i]^numDigits.length;
+			powers += numDigits.length;
+		}
+		
+		return ((long)powers==num);
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,18 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		if (arr != null) {
+		int arrSum = 0;
+		for (int i = 0; i <arr.length; i++ ) {
+			arrSum += arr[i];
+		}
+		System.out.println(arrSum);
+		return arrSum;
+		
+		
+		
+		
 	}
+		return 0;
+}
 }
